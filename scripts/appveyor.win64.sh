7ccfd45c35286bdc4fde1e3bdcddb43114510d0a
Jeroen Asselman <jeroen@asselman.com>
2016-03-20 20:38:47 +0100

    fixup! Build on windows using appveyor

---
+++ scripts/appveyor.win64.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+case "$1" in
+'configure')
+	CPPFLAGS="-DMINGW32"
+	CPPFLAGS="$CPPFLAGS `pkg-config --cflags gtk+-win32-2.0`"
+	CPPFLAGS="$CPPFLAGS `pkg-config --cflags zlib`"
+	
+	./Configure -Oder -D "ccflags=$CPPFLAGS" -D 'libs=-lbfd -liberty -lintl -lpthread -lwsock32 -lws2_32 -liconv -limagehlp -liphlpapi -lws2_32 -lpowrprof -lpsapi -lkernel32
+	;;
+esac
